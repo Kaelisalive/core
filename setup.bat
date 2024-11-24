56c9e0270bab8dbfaa3737ef286db8e88a8903de
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 09:10:06 -0500

    Delete setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-# This is the setup.bat
-# Example content for scripts/setup.bat
-
-# Raw URL: https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/setup.bat
-
-echo 'Executing setup.bat from https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/setup.bat'
