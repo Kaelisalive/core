91c156d1bf82d3743b47d10ec742ede8c3e7bfd1
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 07:53:48 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-# This is the install.sh
-# Example content for scripts/install.sh
-
-# Raw URL: https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/install.sh
-
-echo 'Executing install.sh from https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/install.sh'
