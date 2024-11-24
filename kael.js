fc7175f349fadca41c5571a7ead9b020aafb559a
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 09:09:40 -0500

    Delete kael.js

--- kael.js
+++
@@ -1,6 +0,0 @@
-# This is the kael.js
-# Example content for scripts/kael.js
-
-# Raw URL: https://raw.githubusercontent.com/kaelisalive/core/main/scripts/kael.js
-
-echo 'Executing kael.js from https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/kael.js'
