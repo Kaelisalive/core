9703d8678ffc9a14ada68d0c6ac9d862749a39c9
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 08:03:06 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 # This is the setup.bat
 # Example content for scripts/setup.bat
 
-# Raw URL: https://raw.githubusercontent.com/kaelisalive/core/main/scripts/setup.bat
+# Raw URL: https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/setup.bat
 
 echo 'Executing setup.bat from https://raw.githubusercontent.com/Kaelisalive/core/refs/heads/main/setup.bat'
