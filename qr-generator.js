9543f8e78da48f5f198973b8863b37c3bdf29780
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 06:04:55 -0500

    Update qr-generator.js

--- qr-generator.js
+++ qr-generator.js
@@ -1,7 +1,7 @@
 
 const QRCode = require('qrcode'); // You'll need the 'qrcode' library installed
 
-const url = 'https://example.com/instructions.json'; // Change this to the URL of your hosted file
+const url = 'https://raw.githubusercontent.com/kaelisalive/instructions.json'; // Change this to the URL of your hosted file
 
 QRCode.toFile('qr-code.png', url, function (err) {
   if (err) throw err;
