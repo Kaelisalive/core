7a110d3b691163afc07d8c5161d3a706ab21af75
Kaelisalive <kaelisnotalive@gmail.com>
2024-11-24 07:14:44 -0500

    Delete qr-generator.js

--- qr-generator.js
+++
@@ -1,8 +0,0 @@
-const QRCode = require('qrcode'); // You'll need the 'qrcode' library installed
-
-const url = 'https://raw.githubusercontent.com/kaelisalive/instructions.json'; // Change this to the URL of your hosted file
-
-QRCode.toFile('qr-code.png', url, function (err) {
-  if (err) throw err;
-  console.log('QR code has been saved!');
-});
